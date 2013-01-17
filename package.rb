559e8df07b6a40abb641dcd9289e453b2958e5a9
Mike Stone <stonemj@gmail.com>
2013-01-16 21:35:09 -0800

    Update the packaging script a bit so certain mistakes don't happen again.

--- package.rb
+++ package.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 require "rubygems"
 require "bundler/setup"
 Bundler.require :default
@@ -25,6 +25,7 @@ if TAG
 end
 
 version = ARGS[0]
+die "Please use a version like '1.2.3' (with major.minor.patch)" unless version =~ /\d+\.\d+\.\d+/
 date = %x{ date +"%a %b %_d %H:%M:%S %Y %z" }.strip
 year = %x{ date +"%Y" }.strip
 
@@ -49,10 +50,17 @@ File.open "guards.js", "w" do |f|
   f << header << "\n" << contents
 end
 
+contents = File.read "guards.jquery.json"
+contents.gsub! /"version": "[^"]*"/, %{"version": "#{version}"}
+
+File.open "guards.jquery.json", "w" do |f|
+  f << contents
+end
+
 system "cp guards.js guards-#{version}.js"
 File.open "guards-#{version}.min.js", "w" do |f|
   f << header
   f << Uglifier.compile(File.read("guards-#{version}.js"), :copyright => false)
 end
 
-system "git tag -a #{version} -m 'Version #{version}' && git push --tags" if TAG
+system "git tag #{version} && git push --tags" if TAG
