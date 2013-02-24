9c05736f5f6e8f77371b9844feedda732684fd07
Mike Virata-Stone <stonemj@gmail.com>
2013-02-24 12:47:44 -0800

    Updated pages... working on making it a lot better flow.

---
+++ javascripts/show-source.js
@@ -0,0 +1,25 @@
+function stripWhitespaceColumn(text) {
+    var column = "";
+
+    for (var i = 0; i < text.length; i++) {
+        if (text.charAt(i) == "\n") {
+            column = "";
+        } else if (text.charAt(i) == " ") {
+            column += " ";
+        } else {
+            break;
+        }
+    }
+
+    return $.trim(text.replace(new RegExp("^" + column, "gm"), ""));
+}
+
+$(function() {
+    $(".example .display").each(function() {
+        var $this = $(this);
+        var text = stripWhitespaceColumn($this.html());
+        var $content = $("<code />").text(text);
+        $this.parent().prepend($content);
+        $content.wrap("<pre />").wrap('<div class="source" />');
+    });
+});
